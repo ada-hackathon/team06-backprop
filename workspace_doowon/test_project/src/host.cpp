b4430d8e6531886da5dc0286bc61d569d0203d22
Doowon Lee <dwlee627@gmail.com>
2018-10-01 13:39:46 -0400

    input/check.data added

--- workspace_doowon/test_project/src/host.cpp
+++ workspace_doowon/test_project/src/host.cpp
@@ -30,13 +30,257 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "xcl2.hpp"
 #include <iostream>
 #include <vector>
+#include "backprop.h"
+#include <string.h>
+
 
 #define DATA_SIZE 256
 #define COLS 16
+#define EPSILON (1.0e-6)
 
+///// TYPE macros
+// Macro trick to automatically expand TYPE into the appropriate function
+// (S)et (T)ype (A)nd (C)oncatenate
+#define __STAC_EXPANDED(f_pfx,t,f_sfx) f_pfx##t##f_sfx
+#define STAC(f_pfx,t,f_sfx) __STAC_EXPANDED(f_pfx,t,f_sfx)
+// Invoke like this:
+//   #define TYPE int32_t
+//   STAC(write_,TYPE,_array)(fd, array, n);
+// where array is of type (TYPE *)
+// This translates to:
+//   write_int32_t_array(fd, array, n);
+
+int INPUT_SIZE = sizeof(struct bench_args_t);
+
+void run_benchmark( void *vargs ) {
+  struct bench_args_t *args = (struct bench_args_t *)vargs;
+  backprop( args->weights1, args->weights2, args->weights3,
+            args->biases1,  args->biases2,  args->biases3,
+            args->training_data, args->training_targets );
+}
+
+/* Input format:
+%% Section 1
+TYPE[row_size*col_size]: input matrix
+%% Section 2
+TYPE[f_size]: filter coefficients
+*/
+
+void input_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+
+  // Load input string
+  p = readfile(fd);
+
+  s = find_section_start(p,1);
+  STAC(parse_,TYPE,_array)(s, data->weights1, input_dimension*nodes_per_layer);
+
+  s = find_section_start(p,2);
+  STAC(parse_,TYPE,_array)(s, data->weights2, nodes_per_layer*nodes_per_layer);
+
+  s = find_section_start(p,3);
+  STAC(parse_,TYPE,_array)(s, data->weights3, nodes_per_layer*possible_outputs);
+
+  s = find_section_start(p,4);
+  STAC(parse_,TYPE,_array)(s, data->biases1, nodes_per_layer);
+
+  s = find_section_start(p,5);
+  STAC(parse_,TYPE,_array)(s, data->biases2, nodes_per_layer);
+
+  s = find_section_start(p,6);
+  STAC(parse_,TYPE,_array)(s, data->biases3, possible_outputs);
+
+  s = find_section_start(p,7);
+  STAC(parse_,TYPE,_array)(s, data->training_data, training_sets*input_dimension);
+
+  s = find_section_start(p,8);
+  STAC(parse_,TYPE,_array)(s, data->training_targets, training_sets*possible_outputs);
+  free(p);
+}
+
+void data_to_input(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->weights1, input_dimension*nodes_per_layer);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->weights2, nodes_per_layer*nodes_per_layer);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->weights3, nodes_per_layer*possible_outputs);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->biases1, nodes_per_layer);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->biases2, nodes_per_layer);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->biases3, possible_outputs);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->training_data, training_sets*input_dimension);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->training_targets, training_sets*possible_outputs);
+}
+
+/* Output format:
+%% Section 1
+TYPE[row_size*col_size]: solution matrix
+*/
+
+void output_to_data(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  char *p, *s;
+  // Zero-out everything.
+  memset(vdata,0,sizeof(struct bench_args_t));
+  // Load input string
+  p = readfile(fd);
+
+  s = find_section_start(p,1);
+  STAC(parse_,TYPE,_array)(s, data->weights1, input_dimension*nodes_per_layer);
+
+  s = find_section_start(p,2);
+  STAC(parse_,TYPE,_array)(s, data->weights2, nodes_per_layer*nodes_per_layer);
+
+  s = find_section_start(p,3);
+  STAC(parse_,TYPE,_array)(s, data->weights3, nodes_per_layer*possible_outputs);
+
+  s = find_section_start(p,4);
+  STAC(parse_,TYPE,_array)(s, data->biases1, nodes_per_layer);
+
+  s = find_section_start(p,5);
+  STAC(parse_,TYPE,_array)(s, data->biases2, nodes_per_layer);
+
+  s = find_section_start(p,6);
+  STAC(parse_,TYPE,_array)(s, data->biases3, possible_outputs);
+  free(p);
+
+}
+
+void data_to_output(int fd, void *vdata) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->weights1, input_dimension*nodes_per_layer);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->weights2, nodes_per_layer*nodes_per_layer);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->weights3, nodes_per_layer*possible_outputs);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->biases1, nodes_per_layer);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->biases2, nodes_per_layer);
+
+  write_section_header(fd);
+  STAC(write_,TYPE,_array)(fd, data->biases3, possible_outputs);
+
+}
+
+int check_data( void *vdata, void *vref ) {
+  struct bench_args_t *data = (struct bench_args_t *)vdata;
+  struct bench_args_t *ref = (struct bench_args_t *)vref;
+  int has_errors = 0;
+  int i, j;
+  TYPE diff;
+
+  for(i=0; i<input_dimension; i++) {
+    for(j=0; j<nodes_per_layer; j++) {
+      diff = data->weights1[i*nodes_per_layer + j] - ref->weights1[i*nodes_per_layer + j];
+      has_errors |= (diff<-EPSILON) || (EPSILON<diff);
+    }
+  }
+  for(i=0; i<nodes_per_layer; i++) {
+    for(j=0; j<nodes_per_layer; j++) {
+      diff = data->weights2[i*nodes_per_layer + j] - ref->weights2[i*nodes_per_layer + j];
+      has_errors |= (diff<-EPSILON) || (EPSILON<diff);
+    }
+  }
+  for(i=0; i<nodes_per_layer; i++) {
+    for(j=0; j<possible_outputs; j++) {
+      diff = data->weights3[i*possible_outputs + j] - ref->weights3[i*possible_outputs + j];
+      has_errors |= (diff<-EPSILON) || (EPSILON<diff);
+    }
+  }
+  for(i=0; i<nodes_per_layer; i++) {
+    diff = data->biases1[i] - ref->biases1[i];
+    has_errors |= (diff<-EPSILON) || (EPSILON<diff);
+  }
+  for(i=0; i<nodes_per_layer; i++) {
+    diff = data->biases2[i] - ref->biases2[i];
+    has_errors |= (diff<-EPSILON) || (EPSILON<diff);
+  }
+  for(i=0; i<possible_outputs; i++) {
+    diff = data->biases3[i] - ref->biases3[i];
+    has_errors |= (diff<-EPSILON) || (EPSILON<diff);
+  }
+  // Return true if it's correct.
+  return !has_errors;
+}
 using namespace std;
 int main(int argc, char** argv)
 {
+    // doowon: copied from main() in harness.cpp
+    // Parse command line.
+    char *in_file;
+    char *check_file;
+    assert( argc<4 && "Usage: ./benchmark <input_file> <check_file>" );
+    in_file = "input.data";
+    check_file = "check.data";
+    if( argc>1 )
+      in_file = argv[1];
+    if( argc>2 )
+      check_file = argv[2];
+  
+    // Load input data
+    int in_fd;
+    char *data;
+    data = malloc(INPUT_SIZE);
+    assert( data!=NULL && "Out of memory" );
+    in_fd = open( in_file, O_RDONLY );
+    assert( in_fd>0 && "Couldn't open input data file");
+    input_to_data(in_fd, data);
+  
+    // Unpack and call
+    run_benchmark( data );
+  
+    int out_fd;
+    out_fd = open("output.data", O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+    assert( out_fd>0 && "Couldn't open output data file" );
+    data_to_output(out_fd, data);
+    close(out_fd);
+  
+    // Load check data
+    int check_fd;
+    char *ref;
+    ref = malloc(INPUT_SIZE);
+    assert( ref!=NULL && "Out of memory" );
+    check_fd = open( check_file, O_RDONLY );
+    assert( check_fd>0 && "Couldn't open check data file");
+    output_to_data(check_fd, ref);
+  
+    // Validate benchmark results
+    if( !check_data(data, ref) ) {
+      fprintf(stderr, "Benchmark results are incorrect\n");
+      return -1;
+    }
+    free(data);
+    free(ref);
+  
+    printf("Success.\n");
+    // end of main() in harness.cpp
+
+    return 0;
+
     //Allocate Memory in Host Memory
     size_t vector_size_bytes = sizeof(float) * DATA_SIZE;
 
